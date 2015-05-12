42a5832aa8356a693438dbce16de629f587ce125
15westm <15westm@nsboroschools.net>
2015-05-12 10:31:50 -0400

    added comments

--- CPlusPlusTanks/CPlusPlusTanks/Tank.cpp
+++ CPlusPlusTanks/CPlusPlusTanks/Tank.cpp
@@ -1,23 +1,22 @@
 #include "stdafx.h"
 
-void tank_main(int tank_pos, tank_health, tank_shells, tank_angle)
-	class getTank
+void tank_main(int tank_pos, tank_health, tank_shells, tank_angle)  // passing in tank variables
+	class Tank
 	{
 
 		// get values for tank variables
-		std::gettank_health;
-		std::gettank_pos;
-		std::gettank_shells;
-		std::gettank_angle;
-	}
-    class setTank
-	{
-
+		std::get int tank_health;
+		std::get int tank_pos;
+		std::get int tank_shells;
+		std::get int tank_angle;
+		std::get int tank_power;
+	
         /// set values for tank variables
-		std::settank_health;
-		std::settank_pos;
-		std::settank_shells;
-		std::settank_angle;
+		std::set int tank_health;
+		std::set int tank_pos;
+		std::set int tank_shells;
+		std::set int tank_angle;
+        std:set int tank_power;
     
 	}
 /*
