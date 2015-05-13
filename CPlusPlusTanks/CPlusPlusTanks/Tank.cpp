98439bc84da1c885e981a3d2b7251e2066afdcce
adas98 <dasgupta.anushka@yahoo.com>
2015-05-13 10:08:47 -0400

    more editing gets and sets in tank file (comment)

--- CPlusPlusTanks/CPlusPlusTanks/Tank.cpp
+++ CPlusPlusTanks/CPlusPlusTanks/Tank.cpp
@@ -1,6 +1,21 @@
 #include "stdafx.h"
 
 void tank_main(int tank_pos, tank_health, tank_shells, tank_angle)
+
+//class Tank
+//	{	private:
+//		public:
+	//		int getHealth(void);
+	//		int getShells(void);
+	//		int getPos(void);
+	//		float getAngle(void);
+	//		int getPower(void);
+	//		void setHealth(int);
+	//		void setShells(int);
+	//		void setPos(int);
+	//		void setAngle(float);
+	//		void setPower(int);
+//	};
 	class getTank
 	{
 
