3808b344ab4fedd2c7211f7cbde80ab2729e7e98
jporetsky <jporetsky76@gmail.com>
2015-05-15 10:16:18 -0400

    logic

--- CPlusPlusTanks/CPlusPlusTanks/BaseGameLogic.cpp
+++ CPlusPlusTanks/CPlusPlusTanks/BaseGameLogic.cpp
@@ -3,23 +3,33 @@
 
 // Include custom classes
 #include"BaseGameLogic.h"
+#include"Tank.cpp"
+#include"CPlusPlusTanks.cpp"
 #include"Main.h"
-
-using namespace std;
 
 void update() {
 
 	cout << "Game Start";
+	Tank tank1;
+	Tank tank2;
+
 	while(i=1) {
 		int i++
-		tank_health=100;
-		tank1_posx=5;
-		tank1_posy=0
-		tank_shells;
-		tank_angle;
-
+		tank1.tank_health=100;
+		tank1.tank_posx=5;
+		tank1.tank_posy=0
+		tank1.tank_shells=0;
+		tank1.tank_angle=30;
+		
+		tank2.tank_health=100;
+		tank2.tank_posx=95;
+		tank2.tank_posy=0
+		tank2.tank_shells=0;
+		tank2.tank_angle=30;
+		
 		moveBack = -.1;
 		moveForward = .1;
+
 
 	}
 }
--- CPlusPlusTanks/CPlusPlusTanks/BaseGameLogic.h
+++ CPlusPlusTanks/CPlusPlusTanks/BaseGameLogic.h
@@ -1,11 +1,2 @@
 #ifndef BASEGAMELOGIC_H
 #define BASEGAMELOGIC_H
-
-class Tank {
-
-	public:
-		const int moveForward;
-		const int moveBack;
-		const int aimUp;
-		const int aimDown;
-	}
