fefb3255e31697c9dcff96426a0394a93877f098
Sourcelife <g9pazi@gmail.com>
2015-05-22 10:06:45 -0400

    added some crappy logic

--- CPlusPlusTanks/CPlusPlusTanks/BaseGameLogic.cpp
+++ CPlusPlusTanks/CPlusPlusTanks/BaseGameLogic.cpp
@@ -1,13 +1,14 @@
-#include<stdafx.h>
-#include<iostream>
+#include <stdafx.h>
+#include <iostream>
 #include "stdafx.h"
 #include <string>
 #include <windows.h>
 #include <iostream>
 #include <conio.h>
 #include <sstream> 
-#include <math.h> 
+#include <math.h>
 
+using namespace std;
 
 // Include custom classes
 #include"Tank.h"
@@ -20,59 +21,43 @@ double MOVE_FORWARD;
 Tank tank1;
 Tank tank2;
 
-int score;
-
 BaseGameLogic::BaseGameLogic() {
 	init();
 }
 void BaseGameLogic::init() {
-
-	/*
-	tank1 = new Tank();
-	tank2 = new Tank();
-
-	
-	tank1.tank_health=100;
-	tank1.tank_posx=5;
-	tank1.tank_posy=0
-	tank1.tank_shells=0;
-	tank1.tank_angle=30;
-	
-	tank2.tank_health=100;
-	tank2.tank_posx=95;
-	tank2.tank_posy=0
-	tank2.tank_shells=0;
-	tank2.tank_angle=30;
-	
-	
 	MOVE_BACK = -.1;
 	MOVE_FORWARD = .1;
-	*/
+
+	AIM_UP = 5;
+	AIM_DOWN = -5;
+
+	HEALTH_TANK = 100;
+
+	DAMAGE_TANK = 20;
+
+	EXPLOSION_RADIUS = 10;
 }
+
 void BaseGameLogic::update() {
 	// Game Start
-
-	cout << "Hi";
-
-
 }
 
-int aimUp;
-int aimDown;
+void BaseGameLogic::tankForward() {
+	Tank.move(MOVE_FORWARD);
+}
 
-int moveBack;
-int moveForward;
+void BaseGameLogic::tankBack() {
+	Tank.move(MOVE_BACK);
+}
 
-moveBack = -.1;
-moveForward = .1;
+void BaseGameLogic::tankUp() {
+	Tank.setAngle(AIM_UP);
+}
 
-aimUp=5;
-aimDown= 5;
+void BaseGameLogic::tankDown() {
+	Tank.setAngle(AIM_DOWN);
+}
 
-tank_posx+=moveBack;
-tank_posx+=moveForward;
+void BaseGameLogic::tankShoot() {
 
-tank_angle+=aimUp;
-tank_angle+=aimDown;
-
-keyboard();
+}
